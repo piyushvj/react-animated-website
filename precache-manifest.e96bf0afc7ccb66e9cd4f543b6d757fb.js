48e3489d3d7838303a09a91d03a5b8df2fb98d96
piyushvj <er.piyushvj@gmail.com>
2020-08-17 16:15:18 +0530

    Updates

---
+++ precache-manifest.e96bf0afc7ccb66e9cd4f543b6d757fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc84aa2508c0e7fd2748e22406066d1f",
+    "url": "/react-animated-website/index.html"
+  },
+  {
+    "revision": "5e57445b323affaa4608",
+    "url": "/react-animated-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "38ce8b07add44ae1f48e",
+    "url": "/react-animated-website/static/css/main.f61fa64c.chunk.css"
+  },
+  {
+    "revision": "5e57445b323affaa4608",
+    "url": "/react-animated-website/static/js/2.f5325713.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-animated-website/static/js/2.f5325713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ce8b07add44ae1f48e",
+    "url": "/react-animated-website/static/js/main.392286e4.chunk.js"
+  },
+  {
+    "revision": "83cac4a53eaa7112394b",
+    "url": "/react-animated-website/static/js/runtime-main.e92a3e9d.js"
+  },
+  {
+    "revision": "c63c975f3a6cc30f38e1c958dba46fff",
+    "url": "/react-animated-website/static/media/frog.svg.c63c975f.svg"
+  },
+  {
+    "revision": "47aa711d68b5366b2b9ebc42d6fa582b",
+    "url": "/react-animated-website/static/media/radha-krishna.47aa711d.jpg"
+  }
+]);
